8fa76585c7f7ab5d64bc914efefd9d162218b2ed
xiaokang <13148807065@163.com>
2020-09-24 01:57:10 +0800

    部署网关

--- src/main/java/com/hbhb/cw/gateway/Application.java
+++ src/main/java/com/hbhb/cw/gateway/Application.java
@@ -1,8 +1,6 @@
 package com.hbhb.cw.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author dxk
  */
 @RefreshScope
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
 @EnableFeignClients
 @SpringCloudApplication
